e60911e77955589d9e2d61038ea63616619c270e
oblivia simplex <lucca.fraser@gmail.com>
2016-11-16 23:28:07 -0400

    good ol' phylogen

---
+++ src/haskell/Phylogen.hs
@@ -0,0 +1,47 @@
+module Phylogen where
+
+import Hatchery
+import Gadget
+import Data.Elf
+import ElfHelper
+import qualified Data.List as L
+import ARM32
+import ARMParser
+import System.Random
+import Control.Monad.Random
+import qualified Data.ByteString as BS
+import Unicorn
+
+type Chain = [Gadget]
+type Telos = ([Int], [Int])
+
+telos :: Telos
+telos = ([0, 1, 12], [100, 2, 0xdeadbeef])
+
+distance :: Telos -> [Int] -> Int
+distance (idxs,target) out =
+  let focus = map (out !!) idxs
+  in  sum $ map abs $ zipWith (-) focus target
+
+evalChain' :: Emulator Engine -> [Gadget] -> IO Int
+evalChain' uc chain = do
+  let pkChain = unicornPack chain
+  out <- hatchChain uc pkChain
+  let (regs, stack) = (take 16 out, drop 16 out)
+  let f = distance telos regs
+  -- debug
+  putStrLn $ "\n\n=-=-=-= RESULTS =-=-=-=\n\n" ++ showRegisters regs
+  putStrLn $ "\nFitness: " ++ (showHex f) ++ "\n"
+  -- 
+  return f
+
+evalChain :: Emulator Engine -> [Gadget] -> IO Int
+evalChain uc chain = 
+  (distance telos . take 16) <$> hatchChain uc (unicornPack chain)
+
+mate :: RandomGen g => Chain -> Chain -> Rand g Chain
+mate mom dad = (++) <$> flip take mom <*> flip drop dad <$> pivot
+  where pivot = getRandomR (0, length mom - 1)
+
+    
+
