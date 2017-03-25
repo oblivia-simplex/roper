dd324f818979db0f842409a050239985e2c86634
Oblivia Simplex <oblivia@paranoici.org>
2017-03-24 21:35:40 -0300

    added some statistics functions

---
+++ src/roper/statistics.rs
@@ -0,0 +1,13 @@
+
+
+pub fn standard_deviation (v: &Vec<f32>) -> f32 {
+  let m = mean(v);
+  v.iter()
+   .map(|&x| (x - m).powi(2))
+   .sum::<f32>() / v.len() as f32
+}
+
+pub fn mean (v: &Vec<f32>) -> f32 {
+  v.iter()
+   .sum::<f32>() / v.len() as f32
+}
