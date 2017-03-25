5fa506a28c92ea882b8b76c78020dd21f68c6455
Oblivia Simplex <oblivia@paranoici.org>
2017-03-24 21:45:18 -0300

    fixed stddev

--- src/roper/statistics.rs
+++ src/roper/statistics.rs
@@ -2,9 +2,9 @@
 
 pub fn standard_deviation (v: &Vec<f32>) -> f32 {
   let m = mean(v);
-  v.iter()
-   .map(|&x| (x - m).powi(2))
-   .sum::<f32>() / v.len() as f32
+  (v.iter()
+    .map(|&x| (x - m).powi(2))
+    .sum::<f32>() / v.len() as f32).sqrt()
 }
 
 pub fn mean (v: &Vec<f32>) -> f32 {
